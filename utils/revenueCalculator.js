7ce46596484bcdad19a5790e207ccecf647fb29a
jgierlach <jegierlach@gmail.com>
2021-04-21 17:53:48 -0500

    Check to see if the revenue estimates are below 0.

--- utils/revenueCalculator.js
+++ utils/revenueCalculator.js
@@ -2,27 +2,47 @@ const number = new Intl.NumberFormat('en-IN')
 
 const lower = (rank, reviewCount) => {
   rank = Math.log10(rank)
-  return number.format(Math.floor(50028 - 16936 * rank + 0.0808 * reviewCount))
+  const result = Math.floor(50028 - 16936 * rank + 0.0808 * reviewCount)
+  if (result < 0) {
+    return 0
+  }
+  return number.format(result)
 }
 
 const expected = (rank, reviewCount) => {
   rank = Math.log10(rank)
-  return number.format(Math.floor(53283 - 15911 * rank + 0.1436 * reviewCount))
+  const result = Math.floor(53283 - 15911 * rank + 0.1436 * reviewCount)
+  if (result < 0) {
+    return 0
+  }
+  return number.format(result)
 }
 
 const upper = (rank, reviewCount) => {
   rank = Math.log10(rank)
-  return number.format(Math.floor(56539 - 14885 * rank + 0.2064 * reviewCount))
+  const result = Math.floor(56539 - 14885 * rank + 0.2064 * reviewCount)
+  if (result < 0) {
+    return 0
+  }
+  return number.format(result)
 }
 
 const lowerGrocery = (rank, reviewCount) => {
   rank = Math.log10(rank)
-  return number.format(4.446288 - 0.12511 * rank - 0.02139 - Math.pow(rank, 3))
+  const result = 4.446288 - 0.12511 * rank - 0.02139 - Math.pow(rank, 3)
+  if (result < 0) {
+    return 0
+  }
+  return number.format(result)
 }
 
 const upperGrocery = (rank, reviewCount) => {
   rank = Math.log10(rank)
-  return number.format(4.592164 - 0.05986 * rank - 0.01969 - Math.pow(rank, 3))
+  const result = 4.592164 - 0.05986 * rank - 0.01969 - Math.pow(rank, 3)
+  if (result < 0) {
+    return 0
+  }
+  return number.format(result)
 }
 
 const calculateLower = (rank, reviewCount, parentCategory) => {
