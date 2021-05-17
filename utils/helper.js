97e05c22585d82534ccd8032a43850704bfe4888
jgierlach <jegierlach@gmail.com>
2021-05-17 16:26:26 -0500

    Cleaned up and simplified conditional checks.

--- utils/helper.js
+++ utils/helper.js
@@ -1,35 +1,29 @@
 import axios from 'axios'
 
-const findParentCategory = (bestSellersRank) => {
-  if (bestSellersRank === undefined) {
-    return 'No Parent Category'
-  }
-  return bestSellersRank[0].category
-}
-
 const findBestSellersRank = (product) => {
-  if (product === undefined) {
-    return undefined
-  }
-  if (product.bestsellers_rank === undefined) {
+  if (product === undefined || product.bestsellers_rank === undefined) {
     return undefined
   }
   return product.bestsellers_rank
 }
 
+const findParentCategory = (bestSellersRank) => {
+  if (bestSellersRank === undefined || bestSellersRank[0] === undefined || bestSellersRank[0].category === undefined) {
+    return 'No Parent Category'
+  }
+  return bestSellersRank[0].category
+}
+
 const findParentRank = (bestSellersRank) => {
-  if (bestSellersRank === undefined) {
+  if (bestSellersRank === undefined || bestSellersRank[0] === undefined || bestSellersRank[0].rank === undefined) {
     return 'No Parent Rank'
   }
   return bestSellersRank[0].rank
 }
 
 const findChildCategory = (bestSellersRank) => {
-  if (bestSellersRank === undefined) {
-    return 'No Child Category'
-  }
-  if (bestSellersRank.length === 2) {
-    if (bestSellersRank[1].category === undefined) {
+  if (bestSellersRank !== undefined && bestSellersRank.length === 2) {
+    if (bestSellersRank[1] === undefined || bestSellersRank[1].category === undefined) {
       return 'No Child category'
     }
     return bestSellersRank[1].category
@@ -38,11 +32,8 @@ const findChildCategory = (bestSellersRank) => {
 }
 
 const findChildRank = (bestSellersRank) => {
-  if (bestSellersRank === undefined) {
-    return 'No Child Rank'
-  }
-  if (bestSellersRank.length === 2) {
-    if (bestSellersRank[1].rank === undefined) {
+  if (bestSellersRank !== undefined && bestSellersRank.length === 2) {
+    if (bestSellersRank[1] === undefined || bestSellersRank[1].rank === undefined) {
       return 'No Child Rank'
     }
     return bestSellersRank[1].rank
@@ -51,60 +42,60 @@ const findChildRank = (bestSellersRank) => {
 }
 
 const findSellerPage = (product) => {
-  if (product === undefined) {
-    return 'Sold By Amazon'
-  }
-  if (product.message === 'Country Not Found') {
-    return 'Country Not Found'
-  }
-  if (product.buybox_winner === undefined || product.buybox_winner.fulfillment === undefined || product.buybox_winner.fulfillment.third_party_seller === undefined || product.buybox_winner.fulfillment.third_party_seller.link === undefined) {
+  if (product === undefined || product.buybox_winner === undefined || product.buybox_winner.fulfillment === undefined || product.buybox_winner.fulfillment.third_party_seller === undefined || product.buybox_winner.fulfillment.third_party_seller.link === undefined) {
     return 'Sold By Amazon'
   }
   return product.buybox_winner.fulfillment.third_party_seller.link
 }
 
 const findCompanyCountry = async (sellerPage, scrapeForSellerCountry) => {
-  if (sellerPage === 'Country Not Found') {
-    return 'Country Not Found'
-  }
   if (sellerPage === 'Sold By Amazon') {
     return 'Sold By Amazon'
   }
   if (scrapeForSellerCountry) {
-    const location = await axios.get('/api/companylocation', { params: { url: sellerPage } })
-    return location.data.country
+    try {
+      const location = await axios.get('/api/companylocation', { params: { url: sellerPage } })
+      return location.data
+    } catch (err) {
+      console.error(err)
+    }
   }
   return 'N/A'
 }
 
 const findThumbnail = (product) => {
-  if (product === undefined) {
-    return 'https://i.stack.imgur.com/mwFzF.png'
-  }
-  if (product.main_image === undefined) {
+  if (product === undefined || product.main_image === undefined) {
     return 'https://i.stack.imgur.com/mwFzF.png'
   }
   return product.main_image.link
 }
 
 const findReviewCount = (product) => {
-  if (product === undefined) {
-    return 0
-  }
-  if (product.ratings_total === undefined) {
+  if (product === undefined || product.ratings_total === undefined) {
     return 0
   }
   return product.ratings_total
 }
 
 const findReviewScore = (product) => {
-  if (product === undefined) {
+  if (product === undefined || product.rating === undefined) {
     return 'Not rated'
   }
-  if (product.rating === undefined) {
-    return 'Not Rated'
-  }
   return product.rating
+}
+
+const findBrand = (product) => {
+  if (product === undefined || product.brand === undefined) {
+    return 'Brand not found'
+  }
+  return product.brand
+}
+
+const findNumberOfOtherSellers = (product) => {
+  if (product === undefined || product.more_buying_choices === undefined) {
+    return 0
+  }
+  return product.more_buying_choices.length
 }
 
 const findPrice = (product) => {
@@ -125,5 +116,7 @@ module.exports = {
   findReviewCount,
   findReviewScore,
   findBestSellersRank,
-  findPrice
+  findPrice,
+  findBrand,
+  findNumberOfOtherSellers,
 }
